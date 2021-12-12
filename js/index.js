411f12e971069aec929536e88a8306655e12d82d
Patrick Hagège <hagege.patrick@gmail.com>
2021-12-12 11:40:14 +0100

    Iteration 3 works in browser, does'nt pass the tests

--- js/index.js
+++ js/index.js
@@ -4,20 +4,42 @@ function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
   //... your code goes here
+  const price = product.querySelector('.price span').textContent;
+  const quantity = product.querySelector('.product input').value;
+  let subtotalContainer = product.querySelector('.subtotal span');
+  let subtotal = Number(price) * Number(quantity);
+  subtotalContainer.textContent = `${subtotal}`;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  const products = document.querySelectorAll('.product');
+  //console.log("products ", products);
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i]
+    console.log(product);
+    updateSubtotal(product);
+  }
+  
   // ITERATION 3
   //... your code goes here
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    let product = products[i]
+    total += Number(updateSubtotal(product));
+  }
+  const totalContainer = document.querySelector('#total-value span');
+  console.log(totalContainer);
+  totalContainer.innerHTML = `${total}`;
+
 }
 
 // ITERATION 4
